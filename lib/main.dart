65a36d870624924df1e3fcc25e5b3e6f336355f9
Eduardo Chero <eduardochero08@gmail.com>
2025-06-20 01:40:41 -0500

    Integrate BlocProvider and initialize dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:lockitem_movil/screens/account/login_screen.dart';
-import 'package:lockitem_movil/screens/account/signup_screen.dart';
-import 'package:lockitem_movil/screens/main_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lockitem_movil/presentation/bloc/auth_bloc.dart';
+import 'package:lockitem_movil/presentation/screens/account/login_screen.dart';
+import 'package:lockitem_movil/presentation/screens/account/signup_screen.dart';
+import 'package:lockitem_movil/presentation/screens/main_screen.dart';
+import 'injection_container.dart' as di; // dependency injector
+import 'injection_container.dart'; // Importa el sl
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Necesario si usas async en main
+  await di.init(); // Inicializa las dependencias
   runApp(const MyApp());
 }
 
@@ -12,15 +18,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LockItem App',
-      initialRoute: '/home',
-      routes: {
-        //'/login': (context) => const LoginScreen(),
-        //'/signup': (context) => const SignupScreen(),
-        '/home': (context) => const MainScreen(),
-      },
+    return BlocProvider(
+      create: (_) => sl<AuthBloc>(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'LockItem App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        // home: LoginScreen(), // <--- ELIMINA O COMENTA ESTO
+        initialRoute: '/', // <--- Especifica la ruta inicial
+        routes: {
+          '/': (context) => LoginScreen(), // Tu pantalla de inicio
+          '/signup': (context) => SignupScreen(), // Asegúrate de tener esta pantalla importada
+          // '/main': (context) => MainScreen(), // Y esta también, si la usas
+          // ... otras rutas
+        },
+      ),
     );
   }
 }
